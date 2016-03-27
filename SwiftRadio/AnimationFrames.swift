c5b53641d48ccc483002bf4e8257df0c7762104c
Matthew Michael <matthew.fecher@gmail.com>
2016-03-27 11:54:36 -0600

    Updated code to Swift 2.2
    Xcode 7.3 update

--- SwiftRadio/AnimationFrames.swift
+++ SwiftRadio/AnimationFrames.swift
@@ -20,7 +20,7 @@ class AnimationFrames {
             }
         }
         
-        for (var i = 2; i > 0; i--) {
+        for i in 2.stride(to: 0, by: -1) {
             if let image = UIImage(named: "NowPlayingBars-\(i)") {
                 animationFrames.append(image)
             }
