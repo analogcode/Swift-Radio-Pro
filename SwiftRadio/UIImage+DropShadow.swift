8b1f0ead41954943b972adefdeaeac0f7bd1b4c9
Fethi El Hassasna <e.fethi.c@gmail.com>
2016-09-20 15:15:37 +0100

    Update UIImageView extensions to Swift 3

--- SwiftRadio/UIImage+DropShadow.swift
+++ SwiftRadio/UIImage+DropShadow.swift
@@ -13,7 +13,7 @@ extension UIImageView {
     // APPLY DROP SHADOW
     func applyShadow() {
 		let layer           = self.layer
-		layer.shadowColor   = UIColor.blackColor().CGColor
+		layer.shadowColor   = UIColor.black.cgColor
 		layer.shadowOffset  = CGSize(width: 0, height: 1)
 		layer.shadowOpacity = 0.4
 		layer.shadowRadius  = 2
