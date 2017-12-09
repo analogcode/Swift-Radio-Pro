1519e06c2822d01a93e660374a59ef0beeaffc9c
Fethi El Hassasna <e.fethi.c@gmail.com>
2017-12-09 13:40:25 -0500

    Remove LASTFM API infos

--- SwiftRadio/SwiftRadio-Settings.swift
+++ SwiftRadio/SwiftRadio-Settings.swift
@@ -31,18 +31,3 @@ let stationDataURL   = "http://yoururl.com/json/stations.json"
 
 // Set this to "true" to enable the search bar
 let searchable = false
-
-//**************************************
-// LASTFM API
-//**************************************
-
-// Use LastFM or iTunes API
-// set to "false" to use iTunes
-let useLastFM = true
-
-// IF YOU USE LASTFM, PLEASE USE YOUR OWN KEY
-// Visit: http://www.last.fm/api
-
-let apiKey    = "9a267c245324cfa4f887366d497d3dd3"
-let apiSecret = "f1191864d7ae71e580b89238129768b8"
-
