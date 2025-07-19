623a909a15f54ff3bce334cdd0d2bc3ab1a34e0b
Joe McMahon <joe.mcmahon@gmail.com>
2025-07-18 20:34:37 -0700

    Keep websocket alive during background audio playback; bump build to 62 for TestFlight

--- RadioSpiral/AppDelegate.swift
+++ RadioSpiral/AppDelegate.swift
@@ -55,9 +55,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-        ACWebSocketClient.shared.disconnect()
+        // Only disconnect websocket if audio is NOT playing
+        if !FRadioPlayer.shared.isPlaying {
+            ACWebSocketClient.shared.disconnect()
+        }
     }
     
     func applicationDidEnterBackground(_ application: UIApplication) {
