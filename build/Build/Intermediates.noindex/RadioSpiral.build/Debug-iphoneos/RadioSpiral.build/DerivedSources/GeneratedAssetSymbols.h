d7fe36e5def11248d13e011953bb6cd7197ff372
Joe McMahon <joe.mcmahon@gmail.com>
2025-11-11 19:34:45 -0800

    Remove unused SwiftRadio.entitlements file

---
+++ build/Build/Intermediates.noindex/RadioSpiral.build/Debug-iphoneos/RadioSpiral.build/DerivedSources/GeneratedAssetSymbols.h
@@ -0,0 +1,114 @@
+#import <Foundation/Foundation.h>
+
+#if __has_attribute(swift_private)
+#define AC_SWIFT_PRIVATE __attribute__((swift_private))
+#else
+#define AC_SWIFT_PRIVATE
+#endif
+
+/// The "NowPlayingBars" asset catalog image resource.
+static NSString * const ACImageNameNowPlayingBars AC_SWIFT_PRIVATE = @"NowPlayingBars";
+
+/// The "NowPlayingBars-0" asset catalog image resource.
+static NSString * const ACImageNameNowPlayingBars0 AC_SWIFT_PRIVATE = @"NowPlayingBars-0";
+
+/// The "NowPlayingBars-1" asset catalog image resource.
+static NSString * const ACImageNameNowPlayingBars1 AC_SWIFT_PRIVATE = @"NowPlayingBars-1";
+
+/// The "NowPlayingBars-2" asset catalog image resource.
+static NSString * const ACImageNameNowPlayingBars2 AC_SWIFT_PRIVATE = @"NowPlayingBars-2";
+
+/// The "NowPlayingBars-3" asset catalog image resource.
+static NSString * const ACImageNameNowPlayingBars3 AC_SWIFT_PRIVATE = @"NowPlayingBars-3";
+
+/// The "albumArt" asset catalog image resource.
+static NSString * const ACImageNameAlbumArt AC_SWIFT_PRIVATE = @"albumArt";
+
+/// The "az-rock-radio" asset catalog image resource.
+static NSString * const ACImageNameAzRockRadio AC_SWIFT_PRIVATE = @"az-rock-radio";
+
+/// The "background" asset catalog image resource.
+static NSString * const ACImageNameBackground AC_SWIFT_PRIVATE = @"background";
+
+/// The "btn-close" asset catalog image resource.
+static NSString * const ACImageNameBtnClose AC_SWIFT_PRIVATE = @"btn-close";
+
+/// The "btn-next" asset catalog image resource.
+static NSString * const ACImageNameBtnNext AC_SWIFT_PRIVATE = @"btn-next";
+
+/// The "btn-nowPlaying" asset catalog image resource.
+static NSString * const ACImageNameBtnNowPlaying AC_SWIFT_PRIVATE = @"btn-nowPlaying";
+
+/// The "btn-pause" asset catalog image resource.
+static NSString * const ACImageNameBtnPause AC_SWIFT_PRIVATE = @"btn-pause";
+
+/// The "btn-play" asset catalog image resource.
+static NSString * const ACImageNameBtnPlay AC_SWIFT_PRIVATE = @"btn-play";
+
+/// The "btn-previous" asset catalog image resource.
+static NSString * const ACImageNameBtnPrevious AC_SWIFT_PRIVATE = @"btn-previous";
+
+/// The "btn-stop" asset catalog image resource.
+static NSString * const ACImageNameBtnStop AC_SWIFT_PRIVATE = @"btn-stop";
+
+/// The "carPlayTab" asset catalog image resource.
+static NSString * const ACImageNameCarPlayTab AC_SWIFT_PRIVATE = @"carPlayTab";
+
+/// The "icon-hamburger" asset catalog image resource.
+static NSString * const ACImageNameIconHamburger AC_SWIFT_PRIVATE = @"icon-hamburger";
+
+/// The "icon-info" asset catalog image resource.
+static NSString * const ACImageNameIconInfo AC_SWIFT_PRIVATE = @"icon-info";
+
+/// The "logo" asset catalog image resource.
+static NSString * const ACImageNameLogo AC_SWIFT_PRIVATE = @"logo";
+
+/// The "radiospiral" asset catalog image resource.
+static NSString * const ACImageNameRadiospiral AC_SWIFT_PRIVATE = @"radiospiral";
+
+/// The "share" asset catalog image resource.
+static NSString * const ACImageNameShare AC_SWIFT_PRIVATE = @"share";
+
+/// The "slider-ball" asset catalog image resource.
+static NSString * const ACImageNameSliderBall AC_SWIFT_PRIVATE = @"slider-ball";
+
+/// The "station-80s" asset catalog image resource.
+static NSString * const ACImageNameStation80S AC_SWIFT_PRIVATE = @"station-80s";
+
+/// The "station-absolutecountry" asset catalog image resource.
+static NSString * const ACImageNameStationAbsolutecountry AC_SWIFT_PRIVATE = @"station-absolutecountry";
+
+/// The "station-altvault" asset catalog image resource.
+static NSString * const ACImageNameStationAltvault AC_SWIFT_PRIVATE = @"station-altvault";
+
+/// The "station-classicrock" asset catalog image resource.
+static NSString * const ACImageNameStationClassicrock AC_SWIFT_PRIVATE = @"station-classicrock";
+
+/// The "station-killrockstars" asset catalog image resource.
+static NSString * const ACImageNameStationKillrockstars AC_SWIFT_PRIVATE = @"station-killrockstars";
+
+/// The "station-newportfolk" asset catalog image resource.
+static NSString * const ACImageNameStationNewportfolk AC_SWIFT_PRIVATE = @"station-newportfolk";
+
+/// The "station-spaceland" asset catalog image resource.
+static NSString * const ACImageNameStationSpaceland AC_SWIFT_PRIVATE = @"station-spaceland";
+
+/// The "station-sub" asset catalog image resource.
+static NSString * const ACImageNameStationSub AC_SWIFT_PRIVATE = @"station-sub";
+
+/// The "station-therockfm" asset catalog image resource.
+static NSString * const ACImageNameStationTherockfm AC_SWIFT_PRIVATE = @"station-therockfm";
+
+/// The "stationImage" asset catalog image resource.
+static NSString * const ACImageNameStationImage AC_SWIFT_PRIVATE = @"stationImage";
+
+/// The "swift-radio-black" asset catalog image resource.
+static NSString * const ACImageNameSwiftRadioBlack AC_SWIFT_PRIVATE = @"swift-radio-black";
+
+/// The "vol-max" asset catalog image resource.
+static NSString * const ACImageNameVolMax AC_SWIFT_PRIVATE = @"vol-max";
+
+/// The "vol-min" asset catalog image resource.
+static NSString * const ACImageNameVolMin AC_SWIFT_PRIVATE = @"vol-min";
+
+#undef AC_SWIFT_PRIVATE
